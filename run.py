69b4d4d85c6975f991a313684d7833ea30aeb197
User <12345>
2022-11-29 17:31:19 +0300

    начали добавлять рендер объявлений на главной страницы

--- run.py
+++ run.py
@@ -28,7 +28,8 @@ class Ad_table(db.Model):
     type_LBZ = db.Column(db.String)
     server = db.Column(db.String, nullable = False)
     fights_count =   db.Column(db.Integer)
-    rait = db.Column(db.Integer)
+    win_rait = db.Column(db.Integer)
+    raiting = db.Column(db.Integer)
     wn8 = db.Column(db.Integer)
     count =  db.Column(db.Integer)
     price  =  db.Column(db.Integer)
@@ -36,11 +37,27 @@ class Ad_table(db.Model):
 
 @app.route("/")
 def main_page():
-    return render_template("home.html")
+    data=db.session.query(Ad_table).first()
+    data= {"short_desc":data.short_desc,
+           "full_desc":data.full_desc,
+           "type_ad":data.type_ad,
+           "type_bust":data.type_bust,
+           "type_LBZ":data.type_LBZ,
+           "server":data.server,
+           "fights_count":data.fights_count,
+           "win_rait": data.win_rait,
+           "raiting": data.raiting,
+           "wn8": data.wn8,
+           "count": data.count,
+           "price": data.price,
 
-# with app.app_context(): Создает базу данных
+           }
+    print(data)
+    return render_template("home.html", ad=data)
+
+# with app.app_context(): #Создает базу данных
 #     db.create_all()
-#
+
 @app.route("/register")
 def register_page():
 
@@ -68,7 +85,7 @@ def register_page_add():
                     grade =0)
         db.session.add(user)
         db.session.commit()
-        return render_template("home.html")
+        return render_template("home.html", ad={"wn8":"454542"})
     return render_template("regist.html", is_exist=False, false_psw=True)
 
 
